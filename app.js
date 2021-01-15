788c01fca370c2f08d8bb5a179de86102e7296d5
Ben <bbryant@truecoders.io>
2021-01-15 10:51:31 -0600

    Added solution to Answer branch

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ console.log("EXERCISE 1:\n==========\n");
 const http = require("http");
 
 // Finish setting up the server
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("Hello World!");
+  })
+  .listen(3000, function () {
+    console.log("Server listening...");
+  });
