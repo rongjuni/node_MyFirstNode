1a8dd19bc94305ebe2159981b618e95e28f3844b
Matthew Seo <rongjuni@hotmail.com>
2022-05-17 00:44:45 -0400

    complete

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ console.log("EXERCISE 1:\n==========\n");
 
 const http = require("http");
 
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Hello World</h1>");
+    response.end();
+  })
+  .listen(3000, () => {
+    console.log("listening server at 3000");
+  });
+
 // Finish setting up the server
